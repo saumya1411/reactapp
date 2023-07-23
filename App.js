9a17521e3da1b0e35277538a447c09f78f9fd58f
saumya1411 <96434989+saumya1411@users.noreply.github.com>
2023-07-23 22:56:35 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var color = "yellow";
+
+const emojiDictionary = {
+  "😂": "laughing",
+  "😾": "Billi khin  ke",
+  "🐤": "Hello Chicken Singh🌝",
+  "🐢": "You are my Turtle",
+  "🥰": "My Cuteyy",
+  "🥺": "Missing you alot😞",
+  "❤️": " I Love You",
+  "😉": "See you  soon"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    console.log(meaning);
+
+    if (meaning === undefined) {
+      meaning = "I dont know";
+    }
+    setMeaning(meaning);
+  }
+
+  // const [likeCounter, setLikeCounter] = useState(0);
+  // function likeClickHandler(){
+  //  var newlikeCounter = likeCounter + 1;
+  //  setLikeCounter(newlikeCounter);
+  // function inputChangeHandler(event){
+
+  //   console.log(event.target.value)
+  //   setUserInput(event.target.value)
+  // }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1 style={{ backgroundColor: color }}>Inside outt</h1>
+
+      <input onChange={emojiInputHandler} />
+
+      <h2>{meaning}</h2>
+
+      <h3 style={{ fontSize: "2rem" }}>Click on emoji </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "3rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
